723bb3c87d316c5688e7d6c26db75cc46fc0440e
Mahmoud Kassab <mahmoud.kassab@rasid.com.sa>
2024-09-10 11:46:50 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,9 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   console.log("server started");
+  socket.on("data", (data) => {
+    socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+  });
 
   socket.on("close", () => {
     socket.end();
